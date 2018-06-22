90096a9493d9aeb2f7fc2404cf51c4b17c29dca6
KennyBZA <kblewett88@gmail.com>
2018-06-22 10:48:06 +0200

    Update entrypoint.sh
    Corrected SteamCMD path for game update.

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Update Server
 if [ ! -z ${APPID} ]; then
-  ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /home/container +app_update ${APPID} +quit
+  ./steam/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /home/container +app_update ${APPID} +quit
 fi
 
 # Replace Startup Variables
