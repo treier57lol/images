861aac15692ca7c84ec8abba0b82b444b89e88f1
Michael (Parker) Parker <parkervcp@gmail.com>
2018-07-07 09:23:45 -0400

    more source testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,9 +9,9 @@ if [ ! -z ${SRCDS_APPID} ]; then
     ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
 fi
 
-# Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo ":/home/container$ ${MODIFIED_STARTUP}"
+## Replace Startup Variables
+# MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+# echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-eval ${MODIFIED_STARTUP}
+exec ${STARTUP}
