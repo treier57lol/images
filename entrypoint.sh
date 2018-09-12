dce0bb67148c036c4b00163e9202200a15917766
Michael (Parker) Parker <parkervcp@gmail.com>
2018-09-12 10:13:41 -0400

    update ubuntu entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,5 +8,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
+LD_LIBRARY_PATH=.
+
 # Run the Server
 ${MODIFIED_STARTUP}
