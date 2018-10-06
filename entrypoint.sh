9c5b29d543ae666f2405bae4cb08b149903b8fea
Michael (Parker) Parker <parkervcp@gmail.com>
2018-10-06 19:33:30 -0400

    testing changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/container
-
+sleep 1
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
@@ -9,9 +9,9 @@ if [ ! -z ${SRCDS_APPID} ]; then
     ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
 fi
 
-## Replace Startup Variables
-# MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-# echo ":/home/container$ ${MODIFIED_STARTUP}"
+# Replace Startup Variables
+MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-exec ${STARTUP}
+exec ${MODIFIED_STARTUP}
