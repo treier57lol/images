008d017a2f4267ceda3944a52baa6db1058900ac
Michael Parker <parkervcp@gmail.com>
2019-06-14 16:16:07 -0400

    remove mysqld command

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,5 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
-mysqld &
-
 # Run the Server
 ${MODIFIED_STARTUP}
