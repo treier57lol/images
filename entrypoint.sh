9de38d1866138058a1990cd055f72869d1ee0e7e
Michael (Parker) Parker <parkervcp@gmail.com>
2019-07-22 08:47:20 -0400

    add deprecation message to the samp egg

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,11 @@ cd /home/container
 
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+echo -e "The image the server is using is being deprecated and needs to be changes to `quay.io/parkervcp/pterodactyl-images:game_samp`"
+echo -e "If you are renting the server and are seeing this message let your host know soon please."
+
+sleep 15
+
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
