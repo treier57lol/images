b98a460ba24cd046660d1a80337f104ba68d361a
Michael Parker <parkervcp@gmail.com>
2019-09-08 10:58:41 -0400

    trying some new eval changes
    removed an echo from the eval command

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP="eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')"
+MODIFIED_STARTUP="eval $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')"
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
