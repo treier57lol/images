6ecba10c1b7160187817f259d89beb79f063aebf
Michael Parker <parkervcp@gmail.com>
2019-09-29 20:19:47 -0400

    updating image as a test, please ignore
    please ignore

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ cd /home/container
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
+/usr/sbin/mysqld &
+
 # Replace Startup Variables
 MODIFIED_STARTUP=$(eval $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
 echo ":/home/container$ ${MODIFIED_STARTUP}"
