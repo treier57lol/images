34bb20331fdfd84de745431b4dbb551d02693fb0
WGOS <thewgosplayer@gmail.com>
2019-10-25 21:41:29 +0300

    Added necessary steamcmd packages. Added wine-gecko and wine-mono

--- Dockerfile
+++ Dockerfile
@@ -5,21 +5,28 @@
 # ----------------------------------
 FROM		debian:buster-slim
 
-LABEL		author="WGOS"
+LABEL		author="WGOS" maintainer="wgos@wgos.org"
 
 ENV		DEBIAN_FRONTEND noninteractive
 
 # Install Dependencies
 RUN		dpkg --add-architecture i386 \
-		&& apt-get update \
-		&& apt-get upgrade -y \
-		&& apt-get install -y --install-recommends wine64=4.0-2 xvfb \
-		&& useradd -m -d /home/container container
+		&& apt update \
+		&& apt upgrade -y \
+		&& apt install -y --install-recommends wine wine64 \
+		&& apt install -y --no-install-recommends wget curl lib32gcc1 ca-certificates winbind xvfb tzdata \
+		&& useradd -m -d /home/container container \
+		&& mkdir /wine \
+		&& wget -q -O /wine/gecko_x86.msi http://dl.winehq.org/wine/wine-gecko/2.47/wine_gecko-2.47-x86.msi \
+		&& wget -q -O /wine/gecko_x86_64.msi http://dl.winehq.org/wine/wine-gecko/2.47/wine_gecko-2.47-x86_64.msi \
+		&& wget -q -O /wine/mono.msi http://dl.winehq.org/wine/wine-mono/4.9.3/wine-mono-4.9.3.msi \
+		&& chmod -R 0444 /wine
 
 USER		container
 ENV		HOME /home/container
+ENV		DISPLAY :0
 WORKDIR		/home/container
 
 COPY		./entrypoint.sh /entrypoint.sh
-COPY		--chown=container:container ./wineprefix /home/container/.wine
 CMD		["/bin/bash", "/entrypoint.sh"]
+
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,16 +4,30 @@ sleep 1
 
 # Update Application
 if [ ! -z ${APPID} ]; then
-    if [ ! -z ${BETAID} ]; then
-        if [ ! -z ${BETAPASS} ]; then
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} -beta ${BETAID} -betapassword ${BETAPASS} +quit
+        if [ ! -z ${BETAID} ]; then
+                if [ ! -z ${BETAPASS} ]; then
+                        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} -beta ${BETAID} -betapassword ${BETAPASS} +quit
+                else
+                        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} -beta ${BETAID} +quit
+                fi
         else
-            ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} -beta ${BETAID} +quit
+                ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} +quit
         fi
-    else
-        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} +quit
-    fi
 fi
+
+Xvfb :0 -screen 0 1024x768x16 &
+
+echo "First Wine launch will throw some errors. Ignore them"
+
+mkdir .wine
+echo "Installing Gecko"
+wine msiexec /i /wine/gecko_x86.msi /qn /quiet /norestart /log .wine/gecko_x86_install.log
+wine msiexec /i /wine/gecko_x86_64.msi /qn /quiet /norestart /log .wine/gecko_x86_64_inatall.log
+
+echo "Installing mono"
+wine msiexec /i /wine/mono.msi /qn /quiet /norestart /log .wine/mono_install.log
+
+wine --version
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
