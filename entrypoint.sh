b176512f3022a6b1d50be95655428f2c22c0d8e3
Michael Parker <parkervcp@gmail.com>
2019-11-06 18:41:54 -0500

    update entrypoint
    This is to fix some stuff in the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,7 +26,7 @@ else
 fi
 
 # Replace Startup Variables
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
