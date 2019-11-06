b616b1432b592c223fafcf2bed86c5c6d8aee21b
Michael Parker <parkervcp@gmail.com>
2019-11-06 18:41:01 -0500

    update entrypoint.sh
    This is to change to the new start setup I have worked on.

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,8 @@ cd /home/container
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Replace Startup Variables
-MODIFIED_STARTUP=$(eval $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'))
+MODIFIED_STARTUP=$(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
-${MODIFIED_STARTUP}
+eval ${MODIFIED_STARTUP}
