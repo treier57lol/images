df57dd512bf27402f1c9a492bdc5a50452e82da6
parkervcp <parkervcp@gmail.com>
2019-12-15 09:36:27 -0500

    fix steamcmd location
    actually point at steamcmd/steamcmd.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,15 +20,15 @@ if [  -z ${AUTO_UPDATE} ] || [ ${AUTO_UPDATE} == 1 ]; then
     # Update Source Server
     if [ ! -z ${SRCDS_APPID} ]; then
         if [ ! -z ${HLDS_GAME} ]; then
-            ./steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} +app_set_config 90 mod ${HLDS_GAME} +quit
+            ./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} +app_set_config 90 mod ${HLDS_GAME} +quit
         elif [ ! -z ${SRCDS_BETAID} ]; then
             if [ ! -z ${SRCDS_BETAPASS} ]; then
-                ./steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
+                ./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
             else
-                ./steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
+                ./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} -beta ${SRCDS_BETAID} +quit
             fi
         else
-            ./steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
+            ./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH}  +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
         fi
     else
         echo -e "No appid set. Starting Server"
