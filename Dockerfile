3490ae6f04bb496eba26a0ee98d468a58a47b527
Michael Parker <parkervcp@gmail.com>
2020-09-28 21:11:45 -0400

    update wine 5 image
    update to add steamcmd support
    
    also cleans up base image

--- Dockerfile
+++ Dockerfile
@@ -3,28 +3,26 @@
 # Environment: Debian 19 Buster + Wine 5.0
 # Minimum Panel Version: 0.7.15
 # ----------------------------------
-FROM debian:buster-slim
+FROM quay.io/parkervcp/pterodactyl-images:base_debian
 
-LABEL author="Terrahost" maintainer="opensource@terrahost.cloud"
+LABEL author="Michael Parker" maintainer="parker@pterodactyl.io"
 
-ENV DEBIAN_FRONTEND noninteractive
-
-RUN apt install -y --no-install-recommends gnupg2 wget curl software-properties-common libntlm0 winbind xvfb xauth python3
+## install required packages
+RUN dpkg --add-architecture i386 \
+ && apt update -y \
+ && apt install -y --no-install-recommends libntlm0 winbind xvfb xauth python3
 
 # Install winehq-stable and with recommends
-RUN wget -qO - https://dl.winehq.org/wine-builds/winehq.key | apt-key add -
-RUN apt-add-repository https://dl.winehq.org/wine-builds/debian/
-RUN wget -O- -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/Release.key | apt-key add -    
-RUN echo "deb http://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10 ./" | tee /etc/apt/sources.list.d/wine-obs.list
-RUN apt-get update
-RUN apt install -y --install-recommends winehq-stable
+RUN wget -qO - https://dl.winehq.org/wine-builds/winehq.key | apt-key add - \
+ && apt-add-repository https://dl.winehq.org/wine-builds/debian/ \
+ && wget -O- -q https://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10/Release.key | apt-key add - \
+ && echo "deb http://download.opensuse.org/repositories/Emulators:/Wine:/Debian/Debian_10 ./" | tee /etc/apt/sources.list.d/wine-obs.list \
+ && apt-get update \
+ && apt install -y --install-recommends winehq-stable
 
 # Set up Winetricks
 RUN	wget -q -O /usr/sbin/winetricks https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks \
- && chmod +x /usr/sbin/winetricks \
- && echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen \
- && locale-gen \
- && useradd -m -d /home/container container
+ && chmod +x /usr/sbin/winetricks
 
 ENV HOME=/home/container
 ENV WINEPREFIX=/home/container/.wine
--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,30 @@ echo "Running on Debian $(cat /etc/debian_version)"
 echo "Current timezone: $(cat /etc/timezone)"
 wine --version
 
-# Update Application
-if [ ! -z ${APPID} ] && [[ $AUTO_UPDATE == 1 ]]; then
-        if [ ! -z ${APPID_BETAID} ]; then
-                if [ ! -z ${APPID_BETAPASS} ]; then
-                        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} -beta ${APPID_BETAID} -betapassword ${SRCDS_BETAPASS} +quit
-                else
-                        ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} -beta ${APPID_BETAID} +quit
-                fi
-        else
-                ./steamcmd/steamcmd.sh +login anonymous +force_install_dir /home/container +app_update ${APPID} +quit
-        fi
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+## just in case someone removed the defaults.
+if [ "${STEAM_USER}" == "" ]; then
+    echo -e "steam user is not set.\n"
+    echo -e "Using anonymous user.\n"
+    STEAM_USER=anonymous
+    STEAM_PASS=""
+    STEAM_AUTH=""
+else
+    echo -e "user set to ${STEAM_USER}"
+fi
+
+## if auto_update is not set or to 1 update
+if [ -z ${AUTO_UPDATE} ] || [ "${AUTO_UPDATE}" == "1" ]; then 
+    # Update Source Server
+    if [ ! -z ${SRCDS_APPID} ]; then
+        ./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} ${STEAM_AUTH} $( [[ "${WINDOWS_INSTALL}" == "1" ]] && printf %s '+@sSteamCmdForcePlatformType windows' ) +force_install_dir /home/container +app_update ${SRCDS_APPID} $( [[ ! -z ${SRCDS_BETAID} ]] && printf %s "-beta ${SRCDS_BETAID}" ) $( [[ ! -z ${SRCDS_BETAPASS} ]] && printf %s "-betapassword ${SRCDS_BETAPASS}" ) $( [[ ! -z ${HLDS_GAME} ]] && printf %s "+app_set_config 90 mod ${HLDS_GAME}" ) $( [[ ! -z ${VALIDATE} ]] && printf %s "validate" ) +quit
+    else
+        echo -e "No appid set. Starting Server"
+    fi
+else
+    echo -e "Not updating game server as auto update was set to 0. Starting Server"
 fi
 
 if [[ $XVFB == 1 ]]; then
