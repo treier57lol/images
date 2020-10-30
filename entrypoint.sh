3f94fb2c69aa24f4a174a65a8f7cba7267f47047
Torsten Widmann <info@goover.de>
2020-10-30 19:37:25 +0100

    remove validate Files

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Update Server
 if [ ! -z ${SRCDS_APPID} ]; then
-   ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} validate +quit
+   ./steamcmd/steamcmd.sh +@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /home/container +app_update ${SRCDS_APPID} +quit
 fi
 
 #Mod updates
