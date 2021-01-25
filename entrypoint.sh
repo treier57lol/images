217825343033757749e449927f734859482bd190
Red-Thirten <lilkingjr13@yahoo.com>
2021-01-25 14:29:52 -0800

    Update entrypoint.sh
    - Color system worked. Fleshed it out.

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@
 # SteamCMD ID for the Arma 3 GAME (not server). Only used for Workshop mod downloads.
 armaGameID=107410
 # Color Codes
+CYAN='\033[0;36m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
@@ -17,7 +18,7 @@ sleep 1
 
 # Define make mods lowercase function
 ModsLowercase () {
-	echo -e "\n${GREEN}STARTUP: Making mod $1 files/folders lowercase...${NC}"
+	echo -e "\n${GREEN}STARTUP:${NC} Making mod ${CYAN}$1${NC} files/folders lowercase..."
 	for SRC in `find ./$1 -depth`
 	do
 		DST=`dirname "${SRC}"`/`basename "${SRC}" | tr '[A-Z]' '[a-z]'`
@@ -31,15 +32,15 @@ ModsLowercase () {
 # Update dedicated server, if specified
 if [[ ${UPDATE_SERVER} == "1" ]];
 then
-	echo -e "\nSTARTUP: Checking for updates to game server with App ID: ${STEAMCMD_APPID}...\n"
+	echo -e "\n${GREEN}STARTUP:${NC} Checking for updates to game server with App ID: ${CYAN}${STEAMCMD_APPID}${NC}...\n"
 	if [[ -f ./steam.txt ]];
 	then
-		echo -e "\nSTARTUP: steam.txt found in root folder. Using to run SteamCMD script...\n"
+		echo -e "\n${GREEN}STARTUP:${NC} steam.txt found in root folder. Using to run SteamCMD script...\n"
 		./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${STEAMCMD_APPID} ${STEAMCMD_EXTRA_FLAGS} validate +runscript /home/container/steam.txt
 	else
 		./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +force_install_dir /home/container +app_update ${STEAMCMD_APPID} ${STEAMCMD_EXTRA_FLAGS} validate +quit
 	fi
-	echo -e "\nSTARTUP: Game server update check complete!\n"
+	echo -e "\n${GREEN}STARTUP: Game server update check complete!${NC}\n"
 fi
 
 # Download/Update specified Steam Workshop mods, if specified
@@ -47,7 +48,7 @@ if [[ -n ${UPDATE_WORKSHOP} ]];
 then
 	for i in $(echo -e ${UPDATE_WORKSHOP} | sed "s/,/ /g")
 	do
-		echo -e "\nSTARTUP: Downloading/Updating Steam Workshop mod ID: $i...\n"
+		echo -e "\n${GREEN}STARTUP:${NC} Downloading/Updating Steam Workshop mod ID: ${CYAN}$i${NC}...\n"
 		./steamcmd/steamcmd.sh +login ${STEAM_USER} ${STEAM_PASS} +workshop_download_item $armaGameID $i validate +quit
 		# Move the downloaded mod to the root directory, and replace existing mod if needed
 		mkdir -p ./@$i
@@ -57,10 +58,10 @@ then
 		# Make the mods contents all lowercase
 		ModsLowercase @$i
 		# Move any .bikey's to the keys directory
-		echo -e "\nSTARTUP: Moving any mod .bikey files to the ~/keys/ folder...\n"
+		echo -e "\n${GREEN}STARTUP:${NC} Moving any mod .bikey files to the ~/keys/ folder...\n"
 		find ./@$i -name "*.bikey" -type f -exec cp {} ./keys \;
 	done
-	echo -e "\nSTARTUP: Download/Update Steam Workshop mods complete!\n"
+	echo -e "\n${GREEN}STARTUP: Download/Update Steam Workshop mods complete!${NC}\n"
 fi
 
 # Make mods lowercase, if specified
@@ -80,29 +81,29 @@ fi
 # Check if specified server binary exists
 if [[ ! -f ./${SERVER_BINARY} ]];
 then
-	echo -e "\nSTARTUP_ERR: Specified server binary could not be found in files! Verify your Server Binary startup variable."
+	echo -e "\n${RED}STARTUP_ERR: Specified server binary could not be found in files! Verify your Server Binary startup variable.${NC}"
 	exit 1
 fi
 
 # Check if basic.cfg exists, and download if not (Arma really doesn't like it missing for some reason)
 if [[ -n ${BASIC} ]] && [[ ! -f ./${BASIC} ]];
 then
-	echo -e "\nSTARTUP: Specified Basic Network Configuration file \"${BASIC}\" is missing!"
-	echo -e "\tDownloading default file for use instead...\n"
+	echo -e "\n${YELLOW}STARTUP: Specified Basic Network Configuration file \"${CYAN}${BASIC}${YELLOW}\" is missing!${NC}"
+	echo -e "\t${YELLOW}Downloading default file for use instead...${NC}"
 	curl -sSL https://raw.githubusercontent.com/parkervcp/eggs/master/steamcmd_servers/arma/arma3/egg-arma3-config/basic.cfg -o ./${BASIC}
 fi
 
 # Run preflight, if applicable
 if [[ -f ./preflight.sh ]];
 then
-	echo -e "\nSTARTUP: preflight.sh found in root folder. Running preflight...\n"
+	echo -e "\n${GREEN}STARTUP:${NC} preflight.sh found in root folder. Running preflight...\n"
 	./preflight.sh
 fi
 
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
-echo -e "\nSTARTUP: Starting server with the following startup command:"
-echo -e "${MODIFIED_STARTUP}\n"
+echo -e "\n${GREEN}STARTUP:${NC} Starting server with the following startup command:"
+echo -e "${CYAN}${MODIFIED_STARTUP}${NC}\n"
 
 # $NSS_WRAPPER_PASSWD and $NSS_WRAPPER_GROUP have been set by the Dockerfile
 export USER_ID=$(id -u)
@@ -121,6 +122,6 @@ ${MODIFIED_STARTUP}
 
 if [ $? -ne 0 ];
 then
-    echo -e "\nPTDL_CONTAINER_ERR: There was an error while attempting to run the start command.\n"
+    echo -e "\n${RED}PTDL_CONTAINER_ERR: There was an error while attempting to run the start command.${NC}\n"
     exit 1
 fi
