63c72a4bc742e87110127aa2d203b335e0a6f789
Michael Parker <parkervcp@gmail.com>
2021-03-30 20:30:13 -0400

    this is getting annoying

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,4 +9,4 @@ MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
 eval "echo \"${MODIFIED_STARTUP}\""
 
 # Run the Server
-exec /bin/bash -c $(eval "echo \"${MODIFIED_STARTUP}\"") 
+$(eval "echo \"${MODIFIED_STARTUP}\"") 
